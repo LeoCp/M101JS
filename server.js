48954379ca9acf7a828152d94c308d13feb00cb1
LeoCp <leonardocolodette0@gmail.com>
2016-03-19 16:47:11 -0300

    movies

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-var express = require('express')
-,app = express()
-,engines = require('consolidate');
-
-app.engine('html',engines.nunjucks);
-app.set('view engine','html');
-app.set('views',__dirname + '/views');
-
-app.get('/', function (req,res) {
-  res.render('login',{'name':'Login'});
-});
-
-// NOT FOUD
-app.use(function (req,res) {
-  res.sendStatus(404);
-});
+var  MongoClient = require('mongodb').MongoClient
+,engines = require('consolidate')
+,express = require('express')
+,assert =  require('assert');
+app = express();
 
 
-var server = app.listen(3000, function (argument) {
-  var port = server.address().port;
-  console.log('Express server listening on port %s', port);
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+
+    app.get('/', function(req, res){
+
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movies', { 'movies': docs } );
+        });
+
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
 });
